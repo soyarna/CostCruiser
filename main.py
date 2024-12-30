e66d5f350345a9d1fc6b588570aab8e2934d765d
MnsDej <mansdejbo@hotmail.com>
2024-12-30 17:17:48 +0100

    Update electronics route

--- main.py
+++ main.py
@@ -21,10 +21,22 @@ def home():
 def category1():
     with Session() as session:
         result = session.execute(text("""
-            SELECT p.product_name, CAST(p.price AS INT) AS price
+            SELECT 
+	            s.store_name,
+	            p.product_name,
+	            CAST(p.price AS INT) AS price,
+	            CASE
+		            WHEN p.ratings IS NULL THEN 'N/A'
+		            ELSE CAST(p.ratings AS VARCHAR)
+	            END AS ratings,
+	            p.image,
+                p.no_ratings,
+	            p.website_url
             FROM Product AS p
-            INNER JOIN Category AS c ON p.category_id = c.category_id
-            WHERE c.category_id = 1
+        INNER JOIN Category AS c ON p.category_id = c.category_id
+        INNER JOIN Store AS s ON p.store_id = s.store_id
+        WHERE c.category_id = 1
+        ORDER BY p.no_ratings DESC;
         """)).fetchall()
 
         # Skapa HTML-struktur för att visa produkter
@@ -32,8 +44,11 @@ def category1():
         <h1>Electronics</h1>
         <table border="1">
             <tr>
+                <th>Store Name</th>
                 <th>Product Name</th>
                 <th>Price</th>
+                <th>Ratings</th>
+                <th>Image</th>
             </tr>
         """
         
@@ -41,8 +56,11 @@ def category1():
         for row in result:
             html_content += f"""
             <tr>
-                <td>{row.product_name}</td>
+                <td>{row.store_name}</td>
+                <td><a href="{row.website_url}" target="_blank">{row.product_name}</a></td>
                 <td>{row.price}:-</td>
+                <td>{row.ratings}</td>
+                <td><img src="{row.image}" alt="Image coming soon"></td>
             </tr>
             """
         
