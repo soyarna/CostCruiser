4d2f2db374f5642b1f48b64a3b3267807e756a51
MnsDej <mansdejbo@hotmail.com>
2024-12-30 16:02:38 +0100

    Update electronics route products

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from sqlalchemy.orm import sessionmaker
 import pyodbc
-from sqlalchemy import create_engine, URL
+from sqlalchemy import create_engine, URL, text
 
 url = URL.create(drivername="mssql+pyodbc",
                  host="localhost",
                  database="costcruiser",
                  query={"driver": "ODBC Driver 17 for SQL Server"})
 conn = create_engine(url)
+
+Session = sessionmaker(bind=conn)
 
 app = Flask(__name__)
 
@@ -24,9 +27,38 @@ def home():
 
 @app.route('/electronics')
 def category1():
-    return """
-    <h1>ELEKTRONIK</h1>
-    """
+    with Session() as session:
+        result = session.execute(text("""
+            SELECT p.product_name, CAST(p.price AS INT) AS price
+            FROM Product AS p
+            INNER JOIN Category AS c ON p.category_id = c.category_id
+            WHERE c.category_id = 1
+        """)).fetchall()
+
+        # Skapa HTML-struktur för att visa produkter
+        html_content = """
+        <h1>Electronics</h1>
+        <table border="1">
+            <tr>
+                <th>Product Name</th>
+                <th>Price</th>
+            </tr>
+        """
+        
+        # Lägg till produkter i HTML-tabellen
+        for row in result:
+            html_content += f"""
+            <tr>
+                <td>{row.product_name}</td>
+                <td>{row.price}:-</td>
+            </tr>
+            """
+        
+        html_content += "</table>"
+
+    # Returnera HTML-innehållet
+    return html_content
+
 
 @app.route('/outdoor_and_gardening')
 def category2():
