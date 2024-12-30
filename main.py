b02ca141de90d8b1f996dc12525ed4fc8628abc3
MnsDej <mansdejbo@hotmail.com>
2024-12-30 12:01:07 +0100

    test requirements

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
 
-# Koppling till DB och skapa en engine
-
-import pyodbc
-from sqlalchemy import create_engine, URL
 
 url = URL.create(drivername="mssql+pyodbc",
                  host="localhost",
