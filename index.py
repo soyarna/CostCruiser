284e591e3bb6d3594617b1adbe047bcf3e983e04
Neeeroo <amirhasne@hotmail.com>
2025-01-02 12:48:51 +0100

    Fixed Icon to be universal browser

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ def index(title, body):
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <link rel="shortcut icon" type="image/x-icon" href="/static/image/2.png" />
+        <link rel="shortcut icon" type="image/x-icon" href="/static/image/2.ico" />
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{title}</title>
