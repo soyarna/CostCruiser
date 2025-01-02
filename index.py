365fca463dd704cbb9d252f647f6db851e24d7f5
OscarLindenMogard <Oscarlm@hotmail.se>
2025-01-02 11:57:12 +0100

    Get icon to browser tab

--- index.py
+++ index.py
@@ -2,21 +2,22 @@ def index(title, body):
     return f"""
     <!DOCTYPE html>
     <html lang="en">
-        <head>
+    <head>
+        <link rel="shortcut icon" type="image/x-icon" href="/static/image/2.png" />
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>{title}</title>
-            <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
-        </head>
+    </head>
+    <body>
         <h1>Welcome to CostCruiser</h1>
         <h3>Categories:</h3>
+        <nav>
             <a href="/electronics">Electronics</a>
             <a href="/outdoor_and_gardening">Outdoor & Gardening</a>
             <a href="/dog_supplies">Dog supplies</a>
             <a href="/fitness_accessories">Fitness Accessories</a>
             <a href="/home-and-kitchen">Home & Kitchen</a>
-    </html>
-    <body>
+        </nav>
         {body}
     </body>
     </html>
