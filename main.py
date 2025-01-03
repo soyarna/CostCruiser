a707faf008b23ef8d31f30291211c5a825278b20
Neeeroo <amirhasne@hotmail.com>
2025-01-03 22:05:36 +0100

    Creation of register page
    no functions yet other than "Back to Login" and "Back to Shore" button

--- main.py
+++ main.py
@@ -64,3 +64,7 @@ def home_and_kitchen():
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
